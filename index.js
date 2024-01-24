c02d5f0c166456e592bccf300de72bbff96f414b
TylerKnox65 <tylerj652@deltalearns.ca>
2024-01-24 13:21:03 -0800

    Update

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
-const loginForm = document.getElementById("login-form"); 
-document.getElementById("submitButton").addEventListener("click", handleSubmit);
- 
- 
- function Upload() {
+const loginForm = document.getElementById("myForm"); 
+//document.getElementById("getfile").style.display = "none";
+//document.getElementById("submitButton").addEventListener("click", handleSubmit);
+function Upload() {
     document.getElementById("myForm").style.display = "block";
   }
   
-  function closeForm() {
-    alert(js_variable_name)
+function closeForm() {
+    
     document.getElementById("myForm").style.display = "none";
     
   }
-  function handleSubmit(e) {
+function handleSubmit(e) {
+    alert("Login")
     e.preventDefault();
     console.log(e)
     const formData = new FormData(e.target);
     const formProps = Object.fromEntries(formData);
     alert(formData,formProps)
   }
+function getValue(username,password){
+  user = document.getElementById(username).value; 
+  pass = document.getElementById(password).value;
+
+  document.getElementById("myForm").style.display = "none";
+  if (user == "upload" && pass == "1234") {
+    
+    document.getElementById("getfile").style.display = "block";
+  }
+  return false;
+
+}
 
